51f9bc0f1c47012cf5fe9c4b0c9e663c0a25d4bd
rhesna <rhesna1922@gmail.com>
2025-12-16 14:13:04 +0700

    add new action, locator and implement action helper

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,21 +1,31 @@
 package pages;
 
 import factory.DriverFactory;
+import helper.ActionHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     private WebDriver driver = DriverFactory.getDriver();
+    private ActionHelper actionHelper = new ActionHelper(driver);
 
     //Locator Validation
     private final By homePageValidation = By.xpath("//section[@id='slider']//div[@class='item active']//div[1]");
     private final By loggedIn = By.xpath("//li[10]//a[1]");
     private final By deleteAccMessage = By.xpath("//b[normalize-space()='Account Deleted!']");
+    private final By subscriptionTitle = By.xpath("//h2[normalize-space()='Subscription']");
+    private final By subscriptionMessage = By.xpath("//div[@class='footer-widget']//div[@class='row']");
     //Locator Menu
     private final By signupLoginMenu = By.xpath("//a[normalize-space()='Signup / Login']");
+    private final By contactUsMenu = By.xpath("//a[normalize-space()='Contact us']");
+    private final By testCaseMenu = By.xpath("//a[contains(text(),'Test Cases')]");
+    private final By productsMenu = By.xpath("//a[@href='/products']");
     //LocatorButton
     private final By deleteButton = By.xpath("//a[normalize-space()='Delete Account']");
     private final By continueButton = By.xpath("//a[normalize-space()='Continue']");
+    private final By arrowButton = By.xpath("//button[@id='subscribe']");
+    //Locator Field
+    private final By subscriptionField = By.xpath("//input[@id='susbscribe_email']");
 
 
     //Action
@@ -26,9 +36,22 @@ public class HomePage {
     public String getLoggedIn(){
         return driver.findElement(loggedIn).getText();
 
+    //Action Menu
     }
     public void goToSignupLoginMenu(){
         driver.findElement(signupLoginMenu).click();
+    }
+
+    public void goToContactUsMenu(){
+        driver.findElement(contactUsMenu).click();
+    }
+
+    public void goToTestCaseMenu(){
+        driver.findElement(testCaseMenu).click();
+    }
+
+    public void goToProductMenu(){
+        driver.findElement(productsMenu).click();
     }
 
     public String getDeleteAccMessage(){
@@ -42,6 +65,28 @@ public class HomePage {
     public void clickContinue(){
         driver.findElement(continueButton).click();
     }
+
+    public void scrollDownHomePage(){
+        actionHelper.scrollToBottom();
+    }
+
+    public void enterEmailSubscription(String email){
+        driver.findElement(subscriptionField).sendKeys(email);
+    }
+
+    public void clickArrowButton(){
+        driver.findElement(arrowButton).click();
+    }
+
+    public String getSubscriptionTitle(){
+        return driver.findElement(subscriptionTitle).getText();
+    }
+
+    public void verifySubscriptionMessage(){
+        driver.findElement(subscriptionMessage).isDisplayed();
+    }
+
+
 
 
 }
